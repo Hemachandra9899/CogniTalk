23767dc9e67240d779bab0df0e6950a34eb06316
hemachandra Reddy <pottingari@gmailcom>
2025-10-20 13:32:16 +0530

    [feature]: converstions file is added for interaction with the previous chats and curr chats #5

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Ask for a commit message
+echo "enter  the files need to add"
+read file
 echo "Enter commit message:"
 read msg
 
+
 # Run git commands
-git add .
+git add $file
 git commit -m "$msg"
 git push origin main
